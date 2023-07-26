614022694e0fe7a20a22e15961d0ae032eacfcfc
Jalad <jaladshrimali07@gmail.com>
2023-07-26 19:22:23 +0530

    Added bootstrap layouts to the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, only: [:index]
-
+    protect_from_forgery with: :exception
     #before_action :configure_permitted_parameters, if: :devise_controller?
 
     def index
--- app/mailers/property_confirmation_mailer.rb
+++ app/mailers/property_confirmation_mailer.rb
@@ -1,15 +1,32 @@
+# class PropertyConfirmationMailer < ApplicationMailer
+#     default from: 'jalad@gmail.com'
+#     def agent_confirmation(property)
+#         @property = property
+#         @agent_email = User.find_by(id: property.user_id, role: 'agent')&.email        
+#         mail(to: @agent_email, subject: 'Order Confirmation')
+#     end
+
+#     def customer_confirmation(property)
+#         @property = property
+#         @customer = User.find_by(id: @property.user_id, role: 'customer')
+#         @customer_email = @customer.email if @customer
+#         mail(to: @customer_email, subject: 'Order Confirmation')
+#     end
+# end
 class PropertyConfirmationMailer < ApplicationMailer
     default from: 'jalad@gmail.com'
+  
     def agent_confirmation(property)
-        @property = property
-        @agent_email = User.find_by(id: property.user_id, role: 'agent')&.email        
-        mail(to: @agent_email, subject: 'Order Confirmation')
+      @property = property
+      @agent_email = User.find_by(id: property.user_id, role: 'agent')&.email        
+      mail(to: @agent_email, subject: 'Order Confirmation')
     end
-
+  
     def customer_confirmation(property)
-        @property = property
-        @customer = User.find_by(id: @property.user_id, role: 'customer')
-        @customer_email = @customer.email if @customer
-        mail(to: @customer_email, subject: 'Order Confirmation')
+      @property = property
+      @customer = User.find_by(id: @property.user_id, role: 'customer')
+      @customer_email = @customer&.email
+      mail(to: @customer_email, subject: 'Order Confirmation')
     end
 end
+  
