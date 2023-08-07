3f32341a27f5e6d11afa8f258e4e86a0e158671b
Jalad <jaladshrimali07@gmail.com>
2023-08-07 16:09:30 +0530

    Implemented stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,7 +11,13 @@ class PaymentsController < ApplicationController
     # logger.debug "Property ID: #{params[:property_id]}"
     #byebug
     @property = Property.find(params[:id])
-    amount = (@property.price * 0.01 * 100).to_i
+    amount = (@property.price * 0.1 * 100).to_i
+    if (amount > 999999)
+      amount = (@property.price * 0.01 * 100).to_i
+    end
+
+    payment_token = params[:payment_token]
+
   # rescue ActiveRecord::RecordNotFound
   #   flash[:alert] = 'Property not found'
   #   redirect_to properties_path
@@ -19,35 +25,26 @@ class PaymentsController < ApplicationController
     customer = Stripe::Customer.create(
       name: current_user.name,
       email: current_user.email,
-      description: "Customer id: #{current_user.id}"
+      description: "Customer id: #{current_user.id}, name: #{current_user.name}"
     )
 
     payment_intent = Stripe::PaymentIntent.create(
       amount: amount, # Amount in cents
       currency: 'inr',
       customer: customer.id,
-      description: "Payment for Property id: #{@property.id}",
+      description: "Payment for Property id: #{@property.id}, and owner: #{@property.user.name}",
       metadata: {
         property_id: @property.id
       }
     )
-    
 
-    session = Stripe::Checkout::Session.create(
-      payment_intent_data: {
-        setup_future_usage: 'off_session'
-      },
-      customer: customer.id,
-      payment_method_types: ['card'],
-      line_items: [{
-        price: payment_intent.id, # Use the Payment Intent ID as the price
-        quantity: 1
-      }],
-      mode: 'payment',
-      success_url: payments_success_url,
-      cancel_url: payments_cancel_url
+    payment_method = Stripe::PaymentMethod.create(
+      type: 'card',
+      card: {
+        token: params[:stripeToken]
+      }
     )
-    redirect_to session.url
+    render 'layouts/_payment_success', notice: 'Token money transfer successful'
   end
 
   def success
